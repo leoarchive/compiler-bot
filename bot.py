f169a13b65ca541973bd4053dbcc32fc99a30d19
leozamboni <leonardonunes169@gmail.com>
2021-05-09 11:28:40 -0300

    dsf

--- bot.py
+++ bot.py
@@ -9,24 +9,31 @@ with open('config.json') as f:
 bot = commands.Bot(config["prefix"])
 
 
+def check_whitelist(ctx):
+    return str(ctx.message.author.id) in config["whitelist"]
+
+
 @bot.event
 async def on_ready():
     print('All ready {0.user}'.format(bot))
 
 
 @bot.command(aliases=["load"])
+@commands.check(check_whitelist)
 async def load_extension(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
     await ctx.send(f"Extension {extension} loaded successfully!")
 
 
 @bot.command(aliases=["unload"])
+@commands.check(check_whitelist)
 async def unload_extension(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     await ctx.send(f"Extension {extension} unloaded successfully!")
 
 
 @bot.command(aliases=["reload"])
+@commands.check(check_whitelist)
 async def reload_extension(ctx, extension):
     bot.unload_extension(f'cogs.{extension}')
     bot.load_extension(f'cogs.{extension}')
